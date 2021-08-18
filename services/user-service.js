2dd54275fabf21cd9665019c0dbe04012db5ce0f
Bharathi kannan <baranibk13299@gmail.com>
2021-08-18 10:00:31 +0530

    format document

--- services/user-service.js
+++ services/user-service.js
@@ -4,25 +4,24 @@ const basicAuth = 'Basic ' + btoa(dbUsername + ':' + dbPassword);
 
 class UserService {
 
-    static login(email, password){
+    static login(email, password) {
 
         const url = "https://05025f1a-856b-47a0-aadb-52e737a386f3-bluemix.cloudantnosqldb.appdomain.cloud/mobilesalesapp_users/_find"
 
-const requestData = {
-  selector: {
-    email: email,
-    password: password
-  },
-  fields: ["_id", "name", "password"],
-};
+        const requestData = {
+            selector: {
+                email: email,
+                password: password
+            },
+            fields: ["_id", "name", "password"],
+        };
 
-return axios.post(url, requestData, { headers: { Authorization: basicAuth } });
+        return axios.post(url, requestData, { headers: { Authorization: basicAuth } });
 
     }
-    static register(registerValues)
-    {
+    static register(registerValues) {
         const url = "https://05025f1a-856b-47a0-aadb-52e737a386f3-bluemix.cloudantnosqldb.appdomain.cloud/mobilesalesapp_users"
-       return axios.post(url, registerValues, { headers: { 'Authorization': basicAuth } });
+        return axios.post(url, registerValues, { headers: { 'Authorization': basicAuth } });
     }
 }
 
