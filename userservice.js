e798fad3b7652bc0866d81ac4269c86d74b1e54b
harikrishnan <hariece601@gmail.com>
2021-08-18 10:18:42 +0530

    slash and two stars added

--- userservice.js
+++ userservice.js
@@ -2,8 +2,14 @@ const dbUsername = "apikey-v2-237a9fx60g51gyopiewwx5pb339t2r1xw085fzt3skgx";
 const dbPassword = "85e4a7e36372ac1e47c80f4b81a78d62";
 const basicAuth = "Basic " + btoa(dbUsername + ":" + dbPassword);
 class UserService {
+    /**this method added login service layer
+     * 
+     * @param {*} email 
+     * @param {*} password 
+     * @returns 
+     */
     static login(email, password) {
-        
+
         const url = "https://99560248-15e7-4158-bfde-3c13e3ebf4e9-bluemix.cloudantnosqldb.appdomain.cloud/cakeshop_user/_find";
         const loginObj = {
             selector: {
@@ -15,6 +21,11 @@ class UserService {
         };
         return axios.post(url, loginObj, { headers: { Authorization: basicAuth } });
     }
+    /**
+     * this method added register service layer
+     * @param {*} regObj 
+     * @returns 
+     */
     static register(regObj) {
         console.log(regObj);
         console.log(basicAuth);
