002fc40ccba3fa7b4c62a05a85d64ae4f995b5cf
Tamil Selvan K <tamil97mca@gmail.com>
2021-08-18 10:26:08 +0530

    User list added

---
+++ GroceryStoreUser/userList.js
@@ -0,0 +1,18 @@
+class UserList {
+    async getAllUsers() {
+        "User" = [
+            {
+                UserName: "Tamil",
+                MobileNo: 1111111111,
+                Email: "tamil@gmail.com ",
+                Password: "Pass@123"
+            },
+            {
+                UserName: "Renis",
+                MobileNo: 2222222222,
+                Email: "renis@gmail.com ",
+                Password: "Pass@123"
+            }
+        ]
+    }
+}
